aa467eefb8eda839d90990563e14e42f6ca29f3c
iamkelv <moseskelvin4u@gmail.com>
2022-03-30 14:45:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import NavBar from "./components/NavBar";
+
+const Container = styled.div``;
+function App() {
+  return (
+    <Container>
+      <h1>Ecommerce</h1>
+    </Container>
+  );
+}
+
+export default App;
